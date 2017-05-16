1a8c828899ee9aa0b84543b4522cccf04e43ce43
koiketakayuki <koike.takayuki0907@gmail.com>
2017-05-17 08:23:48 +0900

    Update server

--- server/index.js
+++ server/index.js
@@ -14,36 +14,15 @@ const PaymentInformationRequest = Core.recordType('PaymentInformationRequest', [
     dateField('date')
 ]);
 
+const storeConfig = {
+  ip: '127.0.0.1',
+  port: 27017,
+  db: 'ameba',
+  user: 'amebaadmin',
+  password: 'ameba7531',
+};
 
-const store = createStore('127.0.0.1', 27017, 'ameba', 'amebaadmin', 'password');
+const storeService = require('ameba-service')(storeConfig);
 
-// for (var i = 0; i < 1000; ++i) {
-//   const r = createRecord(PaymentInformationRequest, { shopId: 'test', status: '受領', contactPerson: '小池貴之', date: new Date(2017, 5, i) });
-//   store.create(r);
-// }
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
- 
-//ルーティング設定
-app.post('/read/payment-information-request', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  console.log(req.body.option);
-  store.read(PaymentInformationRequest, req.body.condition, req.body.option).then(result => res.send(result));
-});
-
-app.post('/update/payment-information-request', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  store.update(PaymentInformationRequest, req.body.condition, req.body.values).then(result => res.send(result));
-});
- 
-//以下、ルーティング情報があればここに追記していく
-app.listen(3000);
-
-console.log('Server running at http://localhost:3000/');
+storeService.register(PaymentInformationRequest);
+storeService.start(3000);
